840b4066803946e8cb8054b9cad5bffd6cba6b17
quentin <quentin.destrade@gmail.com>
2023-09-12 21:30:54 +0200

    Commit 1
    Add PHP HTML & CSS for Single Page

--- wp-content/themes/confiturecocotte/single-confiture.php
+++ wp-content/themes/confiturecocotte/single-confiture.php
@@ -1,19 +1,62 @@
 <?php get_header(); ?>
 
-<h1 class="site__heading"><?php post_type_archive_title(); ?></h1>
+<main class="product-page">
+	<?php if (have_posts()) : while (have_posts()) : the_post();
+			$image = get_field('image');
 
-<main class="site__portfolio">
-	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-    	<div class="project">
-        	<h2 class="project__title">
-                <a href="<?php the_permalink(); ?>">
-                	<?php the_title(); ?>
-                </a>
-            </h2>
-         	<?php the_post_thumbnail(); ?>
-    	</div>
-    <?php endwhile; endif; ?>
-</main> 
+	?>
+			<div class="product-page__header">
+				<h1 class="product__title"><?php the_title(); ?></h1>
+			</div>
 
-<?php the_posts_pagination(); ?>
+			<div class="product-content">
+				<?php if ($image) : ?>
+					<div class="product-image">
+						<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+					</div>
+				<?php endif; ?>
+
+				<div class="product-details">
+
+					<p class="product-description"><?php the_field('description'); ?></p>
+
+					<p class="product-ingredients">
+						<?php
+						$ingredients = get_field('ingredients');
+
+						if ($ingredients) :
+							$ingredients_list = explode(' ', $ingredients);
+							if (!empty($ingredients_list)) : ?>
+					<p class="product-ingredients">Ingrédients:</p>
+					<ul>
+						<?php foreach ($ingredients_list as $ingredient) : ?>
+							<li><?php echo esc_html($ingredient); ?></li>
+						<?php endforeach; ?>
+					</ul>
+			<?php endif;
+						endif;
+			?>
+			</p>
+
+			<p class="product-price"><?php the_field('prix'); ?> €</p>
+
+			<div class="product-quantity">
+				Quantité: <input type="number" min="1" value="1">
+				<button class="add-to-cart">Ajouter au panier</button>
+			</div>
+			</div>
+
+			
+			<section class="product-reviews">
+				
+			</section>
+
+			<section class="related-products">
+				
+			</section>
+
+	<?php endwhile;
+	endif; ?>
+</main>
+
 <?php get_footer(); ?>
